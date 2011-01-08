a4265bd66b11858aaf14030aa01edbcfaf4026da
Ben Johnson <benbjohnson@yahoo.com>
2011-01-08 16:52:35 -0700

    Refactor FakeWeb stubs

--- spec/loader/twitter_github_loader_spec.rb
+++ spec/loader/twitter_github_loader_spec.rb
@@ -16,12 +16,14 @@ describe Grapevine::TwitterGitHubLoader do
       FakeWeb.clean_registry
   end
 
-  def register_topsy_search_uri(filename, page=1, perpage=100)
-    FakeWeb.register_uri(:get, "http://otter.topsy.com/search.json?page=#{page}&perpage=#{perpage}&window=h&q=site%3Agithub.com", :response => IO.read("#{@fixtures_dir}/topsy/search/#{filename}"))
+  def register_topsy_search_uri(filename, options={})
+    options = {:page=>1, :perpage=>100}.merge(options)
+    FakeWeb.register_uri(:get, "http://otter.topsy.com/search.json?page=#{options[:page]}&perpage=#{options[:perpage]}&window=h&q=site%3Agithub.com", :response => IO.read("#{@fixtures_dir}/topsy/search/#{filename}"))
   end
   
-  def register_topsy_trackback_uri(url, filename, page=1, perpage=100)
-    FakeWeb.register_uri(:get, "http://otter.topsy.com/trackbacks.json?perpage=#{perpage}&window=hour&url=#{CGI.escape(url)}&page=#{page}", :response => IO.read("#{@fixtures_dir}/topsy/trackbacks/#{filename}"))
+  def register_topsy_trackback_uri(url, filename, options={})
+    options = {:page=>1, :perpage=>100}.merge(options)
+    FakeWeb.register_uri(:get, "http://otter.topsy.com/trackbacks.json?perpage=#{options[:perpage]}&window=hour&url=#{CGI.escape(url)}&page=#{options[:page]}", :response => IO.read("#{@fixtures_dir}/topsy/trackbacks/#{filename}"))
   end
   
   def register_github_uri(username, repo_name)
@@ -48,8 +50,8 @@ describe Grapevine::TwitterGitHubLoader do
   end
 
   it 'should support search paging' do
-    register_topsy_search_uri('site_github_page1', 1, 100)
-    register_topsy_search_uri('site_github_page2', 2, 2)
+    register_topsy_search_uri('site_github_page1', :page => 1)
+    register_topsy_search_uri('site_github_page2', :page => 2, :perpage => 2)
     register_topsy_trackback_uri('https://github.com/ginatrapani/ThinkUp/wiki/Installing-ThinkUp-on-Amazon-EC2', 'ginatrapani_ThinkUp')
     register_topsy_trackback_uri('https://github.com/basho/riak/raw/riak-0.14.0/releasenotes/riak-0.14.0.txt', 'basho_riak')
     register_github_uri('basho', 'riak')
@@ -91,8 +93,8 @@ describe Grapevine::TwitterGitHubLoader do
 
   it 'should support trackback paging' do
     register_topsy_search_uri('site_github_single')
-    register_topsy_trackback_uri('https://github.com/ginatrapani/ThinkUp/wiki/Installing-ThinkUp-on-Amazon-EC2', 'ginatrapani_ThinkUp_page1', 1)
-    register_topsy_trackback_uri('https://github.com/ginatrapani/ThinkUp/wiki/Installing-ThinkUp-on-Amazon-EC2', 'ginatrapani_ThinkUp_page2', 2, 2)
+    register_topsy_trackback_uri('https://github.com/ginatrapani/ThinkUp/wiki/Installing-ThinkUp-on-Amazon-EC2', 'ginatrapani_ThinkUp_page1', :page => 1)
+    register_topsy_trackback_uri('https://github.com/ginatrapani/ThinkUp/wiki/Installing-ThinkUp-on-Amazon-EC2', 'ginatrapani_ThinkUp_page2', :page => 2, :perpage => 2)
     register_github_uri('ginatrapani', 'ThinkUp')
     
     @loader.load()
